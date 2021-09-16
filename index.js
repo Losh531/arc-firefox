31511b364f8f4ef271425a77002e2be07d22663b
Losh531 <65097694+Losh531@users.noreply.github.com>
2021-09-16 17:47:11 -0400

    more stuff

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const delay = require('delay');
 const puppeteer = require('puppeteer')
 
-(async function example() {
+;(async () => {
 
   try {
-    for (i == 0; i < 100; i++){
-    await delay(Math.floor((Math.random() * 4000) + 2000));
-    const browser = await puppeteer.launch({product: 'firefox', args: ['--no-sandbox', '--disable-setuid-sandbox']});
-    const page = await browser.newPage();
-    await page.goto('https://arcio-server.losh531.repl.co');
-    await console.log("At Website " + i)
+    for (i = 0; i < 100; i++){
+      await delay(Math.floor((Math.random() * 4000) + 2000));
+      const browser = await puppeteer.launch({product: 'firefox', args: ['--no-sandbox', '--disable-setuid-sandbox']});
+      const page = await browser.newPage();
+      await page.goto('https://arcio-server.losh531.repl.co');
+      await console.log("At Website " + i)
     }
   } finally {
     await console.log("ending")
