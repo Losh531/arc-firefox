c66898227faf93edcafd4a539b7ccd8d1fc3c082
Losh531 <65097694+Losh531@users.noreply.github.com>
2021-09-16 17:41:22 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const delay = require('delay');
+const puppeteer = require('puppeteer')
+
+(async function example() {
+
+  try {
+    for (i == 0; i < 100; i++){
+    await delay(Math.floor((Math.random() * 4000) + 2000));
+    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+    const page = await browser.newPage();
+    await page.goto('https://arcio-server.losh531.repl.co');
+    await console.log("At Website " + i)
+    }
+  } finally {
+    await console.log("ending")
+  }
+})();
+
